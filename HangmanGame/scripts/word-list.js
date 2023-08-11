bc06fb9e8dad78a2151bac2f983bf20b61c72cb2
DevikaM123 <103141719+DevikaM123@users.noreply.github.com>
2023-08-12 00:11:49 +0530

    Add files via upload

---
+++ HangmanGame/scripts/word-list.js
@@ -0,0 +1,43 @@
+const wordList = [
+    {
+        word : "guitar",
+        hint : "A musical instrument with string"
+    },
+    {
+        word : "oxygen",
+        hint : "A colorless, odorless gas essential for life"
+    },
+    {
+        word : "mountain",
+        hint : "A large natural elevation of the earth's surface"
+    },
+    {
+        word : "painting",
+        hint : "An art form using colors on a surface to create images or expression"
+    },
+    {
+        word : "astronomy",
+        hint : "The scientific study of celestial object and phenomena"
+    },
+    {
+        word : "Science",
+        hint : "The systamatic study of the structure and behaviour of the physical and chemical compositon"
+    },
+    {
+        word : "bicycle",
+        hint : "A human-powered vehicle with two wheel"
+    },
+    {
+        word : "sunset",
+        hint : "The daily disapperance of the sun below the horizon"
+    },
+    {
+        word : "coffee",
+        hint : "A popular caffeinated beverage made from roasted coffee beans"
+    },
+    {
+        word : "Dance",
+        hint : "A rythematic movement of the boby often performed to music"
+    }
+
+]
